90d70d9bee308eb7dd103ba419d40966b188f34f
Cerek <cerek.shi@gmail.com>
2024-03-11 15:11:39 -0700

    Add My Department Ticket back-end views.

--- ticket/permissions.py
+++ ticket/permissions.py
@@ -5,3 +5,11 @@ class ViewMyTicketPermission(DjangoModelPermissions):
     def __init__(self):
         self.perms_map = copy.deepcopy(self.perms_map)
         self.perms_map['GET'] = ['%(app_label)s.view_ticket_personal']
+
+
+class ViewMyDeptTicketPermission(DjangoModelPermissions):
+    def __init__(self):
+        self.perms_map = copy.deepcopy(self.perms_map)
+        self.perms_map['GET'] = ['%(app_label)s.view_todo_ticket_dept']
+        self.perms_map['PUT'] = ['%(app_label)s.update_ticket_department']
+        self.perms_map['PATCH'] = ['%(app_label)s.update_ticket_department']
